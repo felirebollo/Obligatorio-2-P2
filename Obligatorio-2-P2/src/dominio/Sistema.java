ee67585e1816842b4e85d43ae9f3145a1f821697
Felirebollo <felipeerebollo@gmail.com>
2026-06-03 08:17:18 -0300

    commit

---
+++ Obligatorio-2-P2/src/dominio/Sistema.java
@@ -0,0 +1,168 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author felipe
+ */
+public class Sistema {
+
+    private ArrayList<Cliente> clientes;
+    private ArrayList<Funcionario> funcionarios;
+    private ArrayList<Paquete> paquetes;
+    private ArrayList<Envio> envios;
+    private ArrayList<Tarifa> tarifas;
+
+    public Sistema(ArrayList<Cliente> clientes, ArrayList<Funcionario> funcionarios, ArrayList<Paquete> paquetes, ArrayList<Envio> envios, ArrayList<Tarifa> tarifas) {
+
+        this.setClientes(clientes);
+        this.setFuncionarios(funcionarios);
+        this.setPaquetes(paquetes);
+        this.setEnvios(envios);
+        this.setTarifas(tarifas);
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public ArrayList<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(ArrayList<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+    public ArrayList<Paquete> getPaquetes() {
+        return paquetes;
+    }
+
+    public void setPaquetes(ArrayList<Paquete> paquetes) {
+        this.paquetes = paquetes;
+    }
+
+    public ArrayList<Envio> getEnvios() {
+        return envios;
+    }
+
+    public void setEnvios(ArrayList<Envio> envios) {
+        this.envios = envios;
+    }
+
+    public ArrayList<Tarifa> getTarifas() {
+        return tarifas;
+    }
+
+    public void setTarifas(ArrayList<Tarifa> tarifas) {
+        this.tarifas = tarifas;
+    }
+    
+    
+    
+    
+    
+    public void agregarCliente(Cliente cliente) {
+        this.getClientes().add(cliente);
+    }
+    
+    public void agregarFuncionario(Funcionario funcionario) {
+        this.getFuncionarios().add(funcionario);
+    }
+    
+    public void agregarPaquete(Paquete paquete) {
+        this.getPaquetes().add(paquete);
+    }
+    
+    public void agregarEnvio(Envio envio) {
+        this.getEnvios().add(envio);
+    }   
+    
+    public void agregarTarifa(Tarifa tarifa) {
+        this.getTarifas().add(tarifa);
+    }
+    
+    public Cliente buscarCliente(String correo) {
+        Cliente clienteBuscado = null;
+        int i = 0;
+
+        while (i < this.getClientes().size() && clienteBuscado == null) {
+            Cliente cliente = this.getClientes().get(i);
+
+            if (cliente.getCorreo().equals(correo)) {
+                clienteBuscado = cliente;
+            }
+
+            i = i + 1;
+        }
+
+        return clienteBuscado;
+    }
+    
+    public Funcionario buscarFuncionario(int numeroFuncionario) {
+        Funcionario funcionarioBuscado = null;
+        int i = 0;
+
+        while (i < this.getFuncionarios().size() && funcionarioBuscado == null) {
+
+            Funcionario funcionario = this.getFuncionarios().get(i);
+
+            if (funcionario.getNumeroFuncionario() == numeroFuncionario) {
+                funcionarioBuscado = funcionario;
+            }
+
+            i = i + 1;
+        }
+
+        return funcionarioBuscado;
+    }
+    
+    public Paquete buscarPaquete(String identificador) {
+        Paquete paqueteBuscado = null;
+        int i = 0;
+
+        while (i < this.getPaquetes().size() && paqueteBuscado == null) {
+
+            Paquete paquete = this.getPaquetes().get(i);
+
+            if (paquete.getIdentificador().equals(identificador)) {
+                paqueteBuscado = paquete;
+            }
+
+            i = i + 1;
+        }
+
+        return paqueteBuscado;
+    }
+    
+    public Tarifa buscarTarifa(String zona) {
+        Tarifa tarifaBuscada = null;
+        int i = 0;
+
+        while (i < this.getTarifas().size() && tarifaBuscada == null) {
+
+            Tarifa tarifa = this.getTarifas().get(i);
+
+            if (tarifa.getZona().equals(zona)) {
+                tarifaBuscada = tarifa;
+            }
+
+            i = i + 1;
+        }
+
+        return tarifaBuscada;
+    }
+    
+    
+    
+    
+}
